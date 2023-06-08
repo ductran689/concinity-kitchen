3122e6e36b2e1d62c0f29115b2b3852bfb68b6c3
ductran689 <td.duc89@gmaill.com>
2023-06-08 22:55:53 +1000

    fix serviceSlug1

--- pages/services/[serviceSlug].js
+++ pages/services/[serviceSlug].js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+/* import { useRouter } from 'next/router'; */
 import React from 'react';
 import Layout from '../../components/Layout';
 import { getAllPics } from '../../prisma/admin';
