7f48e79408dd1a23342a3826434b9a7eabbf9001
ductran689 <td.duc89@gmaill.com>
2023-06-15 08:13:06 +1000

    test bug SubService

--- pages/services/[serviceSlug]/[subSlug].js
+++ pages/services/[serviceSlug]/[subSlug].js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../../../components/Layout';
-import Image from 'next/image';
+/* import Image from 'next/image'; */
 import data from '../../../utils/data';
 import { useRouter } from 'next/router';
 
